5c17d2161af0d32317927afc46a8cc69eed7117f
Eugene APTEKAR <eaptekar@e1r6p8.unit.ua>
2018-07-27 19:21:47 +0300

    get and printf coordinates

--- begin.c
+++ begin.c
@@ -6,7 +6,7 @@
 /*   By: eaptekar <eaptekar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/15 16:40:59 by eaptekar          #+#    #+#             */
-/*   Updated: 2018/07/27 17:55:06 by eaptekar         ###   ########.fr       */
+/*   Updated: 2018/07/27 19:19:10 by eaptekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ t_map	map_create(fd)
 		map.height++;
 	}
 	map.p_amount = map.width * map.height;
-	map = map_get_points(map, list);     //last changes
+	map = map_get_points(map, list);
 	return (map);
 }
 
@@ -68,10 +68,10 @@ size_t		i;
 	while (i < map.p_amount)
 	{
 		p = map.points[i];
-		printf("[%zu] (%d, %d, %d) %d\n", i, p.x, p.y, p.z, p.colour);
+		printf("[%zu] (%d, %d, %d) %X\n", i, p.x, p.y, p.z, p.colour);
 		i++;
 	}
 
-	draw_map();
+//	draw_map();
 	return (0);
 }
--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: eaptekar <eaptekar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/24 16:32:50 by eaptekar          #+#    #+#             */
-/*   Updated: 2018/07/27 17:55:55 by eaptekar         ###   ########.fr       */
+/*   Updated: 2018/07/27 19:19:24 by eaptekar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,20 @@ t_point	newpoint(int x, int y, int z, int colour)
 	return (point);
 }
 
-t_map	map_points(t_map map, t_list *list)
+t_map	map_get_points(t_map map, t_list *list)
 {
 	int	x_p;
 	int	y_p;
-	int	z_p;
 
-	map.points = (t_point)ft_memalloc(map.p_amount * sizeof(t_point));
-	y_p = map.height;
+	map.points = (t_point*)ft_memalloc(map.p_amount * sizeof(t_point));
+	y_p = map.height - 1;
 	while (y_p >= 0)
 	{
-		x_p = map.width;
+		x_p = map.width - 1;
 		while (x_p >= 0)
 		{
 			map.points[y_p * map.width + x_p] = 
-				newpoint(x_p, y_p, (int*)(list->content), list->color);
+				newpoint(x_p, y_p, *(int*)(list->content), list->color);
 			x_p--;
 			list = list->next;
 		}
@@ -62,18 +61,17 @@ size_t	parse_line(char *line, t_list **list)
 	{
 		width++;
 		x_coord = ft_atoi(*coords);
+		colour = 0;
 		while (**coords)
 		{
 			if (**coords == ',')
 				colour = hex_to_int(*coords);
-			else
-				colour = DEFAULT;
 			(*coords)++;
 		}
-//		free(*coords);
+		if (!colour)
+			colour = DEFAULT;
 		ft_lstadd(list, ft_lstnew(&x_coord, colour, sizeof(int)));
 		++coords;
 	}
-//	free(coords);
 	return (width);
 }
